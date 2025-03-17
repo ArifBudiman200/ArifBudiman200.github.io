aca8f5ed30b04c805628a3e6e737730e36e6e32d
ArifBudiman200 <arifgg701@gmail.com>
2025-03-17 14:40:35 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("books.json")
+    .then(response => response.json())
+    .then(data => {
+        const bookList = document.getElementById("book-list");
+        bookList.innerHTML = data.map(book => `
+            <div class="book">
+                <img src="${book.cover}" alt="${book.title}">
+                <div class="book-title">${book.title}</div>
+                <p>${book.author}</p>
+            </div>
+        `).join("");
+    });
+});
+
+function searchBook() {
+    let input = document.getElementById("searchBar").value.toLowerCase();
+    let books = document.querySelectorAll(".book");
+
+    books.forEach(book => {
+        let title = book.querySelector(".book-title").textContent.toLowerCase();
+        book.style.display = title.includes(input) ? "block" : "none";
+    });
+}
