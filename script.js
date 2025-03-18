e53672e2dfbc12c5a65d8ddf9d1f6949c9063dce
ArifBudiman200 <arifgg701@gmail.com>
2025-03-18 13:49:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -60,31 +60,4 @@ window.onload = displayBooks;
 
 let index = 0;
 
-function showSlide(n) {
-    let slides = document.querySelector(".slides");
-    let totalSlides = slides.children.length;
-    
-    if (n >= totalSlides) {
-        index = 0;
-    } else if (n < 0) {
-        index = totalSlides - 1;
-    } else {
-        index = n;
-    }
 
-    let offset = -index * 100 + "%";
-    slides.style.transform = "translateX(" + offset + ")";
-}
-
-function nextSlide() {
-    showSlide(index + 1);
-}
-
-function prevSlide() {
-    showSlide(index - 1);
-}
-
-// Auto-slide setiap 5 detik
-setInterval(() => {
-    nextSlide();
-}, 5000);
